80202a4e49ce5ebdbcfc9e83b06ef61ba58c2de1
Futureman2007 <penner.stephan@gmx.de>
2024-02-18 20:06:58 +0100

    developing

--- files/start_minecraftserver.sh
+++ files/start_minecraftserver.sh
@@ -2,6 +2,7 @@
 
 # GLOBAL VARIABLES
 ## Globals you may edit
+SCREEN_HOME="/usr/bin/"
 SERVER_HOME_FOLDER="{{ server_home_spigot}}"
 SERVER_JARFILE_NAME="{{ server_jarfile_spigot }}"
 PRINT_PREFIX= ">>> "
@@ -17,23 +18,29 @@ WARNING_PREFIX= ">>! Warning: "
 is_mc_server_already_running(){
 	local server_running_state="false"
 	
-	if screen -ls minecraft; then
+	if $SCREEN_HOME/screen -ls minecraft; then
 		server_running_state="true"
-	elif screen -ls spigot; then
+	elif $SCREEN_HOME/screen -ls spigot; then
 		server_running_state="true"
 	fi
 	echo "$server_running_state"
 }
 
 start_server() {
+	if ! $SCREEN_HOME/screen -A -m -d -S minecraft java -Xms1024M -Xmx12G -XX:ParallelGCThreads=4 -XX:MinHeapFreeRatio=5 -XX:MaxHeapFreeRatio=10 -jar $SERVER_HOME_FOLDER/$SERVER_JARFILE_NAME nogui; then
+		return 1
+	fi
+		return 0
+}
+
+main(){
 
 	if is_mc_server_already_running == "false"; then
 		echo $PRINT_PREFIX "Server not running, starting server"
 
-		if ! screen -A -m -d -S minecraft java -Xms1024M -Xmx12G -XX:ParallelGCThreads=4 -XX:MinHeapFreeRatio=5 -XX:MaxHeapFreeRatio=10 -jar $SERVER_HOME_FOLDER/$SERVER_JARFILE_NAME nogui; then
+		if ! start_server; then
 			return 1
 		fi
-			return 0
 
 	elif is_mc_server_already_running == "true"; then
 		echo $PRINT_PREFIX "Server is already running!"
@@ -43,12 +50,7 @@ start_server() {
 		echo $ERROR_PREFIX "Could not determine if minecraft server is already running or not. Something went wrong terribly! Abborting..."
 		return 1
 	fi
-}
-
-main(){
-
-	start_server
-	return $?
+	
 }
 
 main "$@"
